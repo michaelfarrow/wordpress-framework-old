0e4bbcbfe65b009062aaac17a75e974e4d871273
Mike Farrow <contact@mikefarrow.co.uk>
2014-07-07 17:38:39 +0100

    moved common theme aspects to separate repo

--- public/app/themes/roots/functions.php
+++ public/app/themes/roots/functions.php
@@ -2,20 +2,5 @@
 /**
  * Roots includes
  */
-require_once locate_template('/lib/utils.php');           // Utility functions
-require_once locate_template('/lib/init.php');            // Initial theme setup and constants
-require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
-require_once locate_template('/lib/sidebar.php');         // Sidebar class
-require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/titles.php');          // Page titles
-require_once locate_template('/lib/cleanup.php');         // Cleanup
-require_once locate_template('/lib/nav.php');             // Custom nav modifications
-require_once locate_template('/lib/gallery.php');         // Custom [gallery] modifications
-require_once locate_template('/lib/comments.php');        // Custom comments modifications
-require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
-require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
-require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-require_once locate_template('/lib/fields.php');          // Advanced Custom Fields
-require_once locate_template('/lib/custom.php');          // Custom functions
-require_once locate_template('/lib/custom-post-type.php');// Custom post type
-require_once locate_template('/lib/custom-post-types.php');// Custom post types
+require_once locate_template('/common/all.php');             // Utility functions
+require_once locate_template('/custom-post-types.php');             // Utility functions
