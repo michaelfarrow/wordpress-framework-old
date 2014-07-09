ae517e090cfc264961d692ae5f94db7a6f9aabcc
Mike Farrow <contact@mikefarrow.co.uk>
2014-07-09 17:28:07 +0100

    added comments in custom posts file

--- public/app/themes/roots/lib/custom-post-types.php
+++ public/app/themes/roots/lib/custom-post-types.php
@@ -1,4 +1,20 @@
 <?php
 
-// $book = new Custom_Post_Type('Book');
+// See http://melchoyce.github.io/dashicons/ for custom post type icons
+
+// Menu order: (menu_position)
+
+// 5 - below Posts
+// 10 - below Media
+// 15 - below Links
+// 20 - below Pages
+// 25 - below comments
+// 60 - below first separator
+// 65 - below Plugins
+// 70 - below Users
+// 75 - below Tools
+// 80 - below Settings
+// 100 - below second separator
+
+// $book = new Custom_Post_Type('Book', array('menu_icon' => 'dashicons-book'));
 // $book->add_taxonomy('author');
