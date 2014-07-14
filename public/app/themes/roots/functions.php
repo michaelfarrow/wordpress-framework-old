3d6a6716d5cad8e9e03e816a87cce6ca281d98b9
Mike Farrow <contact@mikefarrow.co.uk>
2014-07-14 17:10:24 +0100

    moved around includes, updated common and built

--- public/app/themes/roots/functions.php
+++ public/app/themes/roots/functions.php
@@ -5,7 +5,7 @@
 
 require_once locate_template('lib/config.php');
 require_once locate_template('common/all.php');
-require_once locate_template('lib/custom-post-types.php');
 require_once locate_template('lib/fields.php');
+require_once locate_template('lib/custom-post-types.php');
 require_once locate_template('lib/images.php');
 require_once locate_template('lib/admin.php');
