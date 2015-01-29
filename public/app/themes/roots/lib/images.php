ddb43a419a0fb0b6a62164b582140049308c7fd4
Mike Farrow <contact@mikefarrow.co.uk>
2015-01-29 10:53:22 +0000

    removed outline on focused links and added custom image compression

--- public/app/themes/roots/lib/images.php
+++ public/app/themes/roots/lib/images.php
@@ -1,5 +1,13 @@
 <?php
 
+// Custom Image Quality
+
+function roots_jpeg_quality_callback($arg) {
+   return (int)80;
+}
+
+add_filter('jpeg_quality', 'roots_jpeg_quality_callback');
+
 // Custom image sizes
 
 // add_image_size('custom', 300, 400, true);
