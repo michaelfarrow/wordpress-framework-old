8746fde9c1e82ff6cf44a97ba8a1a10add0c5743
Mike Farrow <contact@mikefarrow.co.uk>
2015-07-17 16:48:59 +0100

    converting to docker

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,12 +1,9 @@
 <?php
 /* Development */
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
-
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
+define('DB_NAME', getenv('WP_DB_NAME'));
+define('DB_USER', getenv('WP_DB_USER'));
+define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
+define('DB_HOST', getenv('WP_DB_HOST') ? getenv('WP_DB_HOST') : 'localhost');
 
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,12 +1,9 @@
 <?php
 /* Staging */
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
-
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
+define('DB_NAME', getenv('WP_DB_NAME'));
+define('DB_USER', getenv('WP_DB_USER'));
+define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
+define('DB_HOST', getenv('WP_DB_HOST') ? getenv('WP_DB_HOST') : 'localhost');
 
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
