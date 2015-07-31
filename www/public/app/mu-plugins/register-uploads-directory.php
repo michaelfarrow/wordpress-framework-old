3e56d013c7ea7114a6a12f3dd8fb5f94e00ab3aa
Mike Farrow <contact@mikefarrow.co.uk>
2015-07-31 01:27:17 +0100

    adding base back

--- www/public/app/mu-plugins/register-uploads-directory.php
+++ www/public/app/mu-plugins/register-uploads-directory.php
@@ -1,6 +1,7 @@
 <?php
 
 add_filter('upload_dir', function($data){
+	$base = '/storage/uploads';
 
 	$data['path'] = $base . substr($data['path'], strpos($data['path'], '/uploads') + 8);
 	$data['basedir'] = $base . substr($data['basedir'], strpos($data['basedir'], '/uploads') + 8);
