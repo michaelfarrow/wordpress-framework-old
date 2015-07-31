69176ad0473fa1299a982ab6a7b6987dfcb4ccd2
Mike Farrow <contact@mikefarrow.co.uk>
2015-07-31 01:07:29 +0100

    get rid of the guff

--- www/public/app/mu-plugins/register-uploads-directory.php
+++ www/public/app/mu-plugins/register-uploads-directory.php
@@ -1,12 +1,6 @@
 <?php
 
 add_filter('upload_dir', function($data){
-	$base = '/storage/uploads';
-
-	if(!is_dir($base)){
-		@mkdir($base);
-		@chown($base, 'www-data');
-	}
 
 	$data['path'] = $base . substr($data['path'], strpos($data['path'], '/uploads') + 8);
 	$data['basedir'] = $base . substr($data['basedir'], strpos($data['basedir'], '/uploads') + 8);
